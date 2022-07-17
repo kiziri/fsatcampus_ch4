d0bedbb2fa02d34ea97f7ba675ecff9a62a23101
kiziri <58429073+kiziri@users.noreply.github.com>
2022-07-17 19:21:47 +0900

    Spring Mybatis - DAO 인터페이스 및 구현 계층 분리

--- src/main/java/com/fastcampus/ch4/dao/BoardDaoImpl.java
+++ src/main/java/com/fastcampus/ch4/dao/BoardDaoImpl.java
@@ -3,13 +3,57 @@ package com.fastcampus.ch4.dao;
 import com.fastcampus.ch4.domain.BoardDto;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Map;
+
+@Repository
 public class BoardDaoImpl implements BoardDao {
     @Autowired SqlSession session;
     private static String namespace = "com.fastcampus.ch4.dao.BoardMapper.";
 
-    @Override
-    public BoardDto select(int bno) {
+    public BoardDto select(Integer bno) throws Exception {
         return session.selectOne(namespace + "select", bno);
+    } // T selectOne(String statement, Object parameter)
+
+    @Override
+    public int delete(Integer bno, String writer) throws Exception {
+        return 0;
+    }
+
+    @Override
+    public int insert(BoardDto dto) throws Exception {
+        return 0;
+    }
+
+    @Override
+    public int update(BoardDto dto) throws Exception {
+        return 0;
+    }
+
+    @Override
+    public int increaseViewCnt(Integer bno) throws Exception {
+        return 0;
+    }
+
+    @Override
+    public List<BoardDto> selectPage(Map map) throws Exception {
+        return null;
+    }
+
+    @Override
+    public List<BoardDto> selectAll() throws Exception {
+        return null;
+    }
+
+    @Override
+    public int deleteAll() throws Exception {
+        return 0;
+    }
+
+    @Override
+    public int count() throws Exception {
+        return 0;
     }
 }
