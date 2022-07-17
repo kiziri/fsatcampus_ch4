55f151110d7efec3db41454b66d1c484a9c01a0d
kiziri <58429073+kiziri@users.noreply.github.com>
2022-07-17 19:19:23 +0900

    Spring Mybatis - 단건 조회 TDD 구현 및 실습

--- src/test/java/com/fastcampus/ch4/dao/BoardDaoImplTest.java
+++ src/test/java/com/fastcampus/ch4/dao/BoardDaoImplTest.java
@@ -1,4 +1,21 @@
-import static org.junit.Assert.*;
+package com.fastcampus.ch4.dao;
+
+import com.fastcampus.ch4.domain.BoardDto;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.Assert.assertTrue;
+
 public class BoardDaoImplTest {
-  
+    @Autowired
+    BoardDao boardDao;
+
+    @Test
+    public void select() throws Exception {
+        assertTrue(boardDao != null);
+        System.out.println("boardDao = " + boardDao);
+        BoardDto boardDto = boardDao.select(1);
+        System.out.println("boardDto = " + boardDto);
+        assertTrue(boardDto.getBno().equals(1));
+    }
 }
